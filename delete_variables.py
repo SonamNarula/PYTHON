3d8fb3dcb11d9afd19e33b1c5205463050214c95
Sonam Narula <sonamnarula2108@gmail.com>
2025-07-16 08:46:01 +0530

    variable deletion

---
+++ delete_variables.py
@@ -0,0 +1,9 @@
+# Assigning value to variable
+x = 10
+print(x)
+
+# Removing the variable using del
+del x
+
+# Trying to print x after deletion will raise an error
+# print(x)  # Uncommenting this line will raise NameError: name 'x' is not defined
